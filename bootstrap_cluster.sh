4e6d588232633e4d887e00da03024feb32ac26cc
lbpowar <60366195+duchaineo1@users.noreply.github.com>
2020-11-27 09:35:42 -0500

    Update bootstrap_cluster.sh

--- bootstrap_cluster.sh
+++ bootstrap_cluster.sh
@@ -78,6 +78,16 @@ lxc file push haproxy.cfg haproxy/etc/haproxy/haproxy.cfg
 lxc exec haproxy -- systemctl enable haproxy
 lxc exec haproxy -- systemctl start haproxy
 
+
+curl -o cfssl https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/darwin/cfssl
+curl -o cfssljson https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/darwin/cfssljson
+
+chmod +x cfssl cfssljson
+
+sudo mv cfssl cfssljson /usr/local/bin/
+
+
+
 echo '[STEP 5 - CA and TLS certificates]'
 
 # CERTIFICATE AUTHORITY
